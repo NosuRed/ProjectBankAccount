38abc1b8cc997603d170d7415824e3b60c1f5b58
NosuRed <31966656+NosuRed@users.noreply.github.com>
2017-10-06 02:44:34 +0200

    Update CheckingAccount.java
    Spelling! :D

--- BANK/src/sample/CheckingAccount.java
+++ BANK/src/sample/CheckingAccount.java
@@ -4,7 +4,7 @@ public class CheckingAccount extends sample.GenericAccount {
     private static final Double CREDITLIMIT = -200.00;
     /**
      * The Pin is set here
-     * the sample.CheckingAccount can borrow money from the back, the is set here
+     * the CheckingAccount can borrow money from the bank
      * the checking account can transfer money!
      * @param secretPin
      */
